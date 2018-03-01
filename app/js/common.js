4d23f6bc2b35b6c8c4dd1577f067b51b5647fba6
Timson <nskvolleytim@gmail.com>
2018-03-01 18:25:22 +0600

    Adaptive carousel for s-brands

--- app/js/common.js
+++ app/js/common.js
@@ -185,9 +185,11 @@ $(function() {
 });
 
 
-	$(".carousel-eq").owlCarousel({
+	$(".carousel-brands").owlCarousel({
 		loop:true,
-
+		margin: 30,
+		nav: true,
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
 		responsive:{
 			0:{
 					items:1,
@@ -209,6 +211,35 @@ $(function() {
 				}
 		}
 	});
+
+
+
+
+	$(".carousel-eq").owlCarousel({
+		loop:true,
+		responsive:{
+			0:{
+					items:1,
+				},
+			520:{
+					items:1,
+				},
+			560:{
+					items:2,
+				},
+			768:{
+					items:2,
+				},
+			992:{
+					items:3,
+				},
+			1200:{
+					items:4,
+				}
+		}
+	});
+
+
 
 
 });
